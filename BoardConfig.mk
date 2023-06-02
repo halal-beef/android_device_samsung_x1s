c1a29698e0f391e69c3e76bd7efef98225fd5d16
Your Name <you@example.com>
2023-06-02 22:16:29 +0100

    Initial lineage 20 tree for x1s (confirmed booting)

---
+++ BoardConfig.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common
+include device/samsung/universal990-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/x1s
+
+# Display
+TARGET_SCREEN_DENSITY := 480
+
+# Kernel
+TARGET_KERNEL_CONFIG := exynos9830-x1sxxx_defconfig
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := x1s
+
+# inherit from the proprietary version
+-include vendor/samsung/x1s/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/x1s
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 3200
+TARGET_SCREEN_WIDTH := 1440
+
+# Overlays 
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
+# Inherit from common
+$(call inherit-product, device/samsung/universal990-common/device-common.mk)
+
+# Inherit from the proprietary version
+$(call inherit-product-if-exists, vendor/samsung/x1s/x1s-vendor.mk)
